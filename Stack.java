4bbf0bb971015b8d5471038b8d1555d5029a282a
michael <michael85731@gmail.com>
2018-03-02 11:19:04 +0800

    change to user interative program, add pop constrain

--- Stack.java
+++ Stack.java
@@ -1,14 +1,18 @@
 abstract class StackStandard
 {
     protected int[] container = null;
-    protected int pointer = 0;
+    protected int pointer = -1;
+    protected int size = 0;
 
     public StackStandard(int size){
         this.container = new int[size];
+        this.size = size;
     }
 
-    public abstract void push(int data);
+    public abstract boolean push(int data);
     public abstract int pop();
+    public abstract boolean isEmpty();
+    public abstract void getStack();
 }
 
 class Stack extends StackStandard
@@ -17,16 +21,33 @@ class Stack extends StackStandard
         super(size);
     }
 
-    public void push(int data){
-        System.out.println("push stack with data " + data);
-        this.container[this.pointer] = data;
-        this.pointer += 1;
+    @Override
+    public boolean push(int data){
+        if(this.pointer < this.size - 1 && this.pointer >= -1){
+            this.pointer += 1;
+            this.container[this.pointer] = data;
+            return true;
+        }else{
+            return false;
+        }
     }
 
+    @Override
     public int pop(){
-        System.out.print("pop stack with data ");
-        this.pointer -= 1;
         int topElement = this.container[this.pointer];
+        this.pointer -= 1;
         return topElement;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return this.pointer < 0 ? true : false;
+    }
+
+    @Override
+    public void getStack(){
+        while(!(isEmpty())){
+            System.out.println(this.pop());
+        }
     }
 }
--- temp.java
+++ temp.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class temp
 {
+    private static boolean flag = true;
+    private static Scanner scanner = new Scanner(System.in);
+    private static Stack stack = null;
+
     public static void main(String args[]){
-        Stack stack = new Stack(10);
-        stack.push(1);
-        stack.push(2);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        System.out.print("Define stack size: ");
+        stack = new Stack(scanner.nextInt());
+
+        System.out.println("Operation code: 1) push data, 2) pop data, 3) check is empty, 4) get stack, exit) termiante the program.");
+
+        while(flag){
+            System.out.print("Please type the operation you want to do: ");
+            manipulate(scanner.next());
+        }
+    }
+
+    public static void manipulate(String operator){
+        String result = "";
+
+        switch(operator){
+            case "1":
+                System.out.print("Input the data you want to push: ");
+                int data = scanner.nextInt();
+                result = stack.push(data) ? "push stack with data " + data : "stack is full";
+                System.out.println(result);
+                break;
+            case "2":
+                System.out.print("pop stack with data ");
+                System.out.println(stack.pop());
+                break;
+            case "3":
+                result = stack.isEmpty() ? "stack is empty" : "stack is not empty";
+                System.out.println(result);
+                break;
+            case "4":
+                if(stack.isEmpty()){
+                    System.out.println("stack is empty now.");
+                    break;
+                }else{
+                    stack.getStack();
+                }
+                break;
+            case "exit":
+                flag = false;
+                break;
+            default:
+                System.out.println("not valid input");
+        }
     }
 }
