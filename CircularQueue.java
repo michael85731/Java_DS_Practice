b6210853627e765974e6d3438d64ab58731f4e4b
michael <michael85731@gmail.com>
2018-03-02 18:39:40 +0800

    add circularQueue structure

--- CircularQueue.java
+++ CircularQueue.java
@@ -1,30 +1,44 @@
 class CircularQueue extends QueueStandard
 {
+    protected boolean fullFlag = false;
+
     public CircularQueue(int size){
         super(size);
+        this.front = 0;
+        this.rear = 0;
     }
 
     @Override
     public void enQueue(int data){
-        this.rear += 1;
         this.container[this.rear] = data;
+        this.rear = (this.rear + 1) % this.size;
+        if(this.rear == this.front){
+            this.fullFlag = true;
+        }else{
+            this.fullFlag = false;
+        }
     }
 
     @Override
     public int deQueue(){
-        this.front += 1;
-        return this.container[this.front];
+        int element = this.container[this.front];
+        this.front = (this.front + 1) % this.size;
+        this.fullFlag = false;
+        return element;
     }
 
     @Override
     public boolean isFull(){
-        return this.rear < this.size - 1 ? false : true;
+        if(this.rear == this.front && this.fullFlag == true){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     @Override
     public boolean isEmpty(){
-        if(this.front == this.rear){
-            this.resetPointer();
+        if(this.front == this.rear && this.fullFlag == false){
             return true;
         }else{
             return false;
@@ -36,10 +50,5 @@ class CircularQueue extends QueueStandard
         while(!(isEmpty())){
             System.out.println(deQueue());
         }
-    }
-
-    private void resetPointer(){
-        this.front = -1;
-        this.rear = -1;
     }
 }
